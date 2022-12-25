76a42ca06f4b08948ff84a2c1f231a8fc4a838f1
DevmithU <79127952+DevmithU@users.noreply.github.com>
2022-12-26 00:58:42 +0530

    server start

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,22 @@
-console.log("server21");
-
 import express from "express";
 import {createServer} from "http";
-
+import {Server} from "socket.io";
+import * as mongoose from "mongoose";
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+mongoose.set('strictQuery', true);
+
+app.get("/",(req, res)=>{
+    res.send("API is UP");
+})
+io.on('connection',()=>{
+    console.log("connect");
+
+});
+mongoose.connect('mongodb://localhost:27017/mdbt1').then(() =>{
+    console.log("connected to mongodb");
+    httpServer.listen(4001,() => {
+        console.log('API is listening on port 4001');
+    });
+});
